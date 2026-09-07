5e43325681a40fcda1a6c8ad88555014a1fc9304
vishalshaw26 <vishalshaw6826@gmail.com>
2026-09-07 10:37:12 +0000

    container

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 261105
+let accountEmail = "vishal@gmail"
+var accountPassword = "223334444" //prefer not to use "var" because of issue in  block scope and functional scope 
+accountCity = "Kolkata"
+let accountState; // undefined
+
+/* now tring yo change all the variables 
+and which one actually changes...*/
+
+//accountId = 2222 I(not allowed)
+accountEmail = "pspsp@gmail"
+accountPassword = "280306"
+accountCity = "Banglore"
+
+console.log(accountId);
+
+//DONT HAVE TO WRITE (console.log) AGAIN and AGAIN
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
