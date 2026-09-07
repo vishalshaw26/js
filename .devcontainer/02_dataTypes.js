b027832f2eecdfed6a101b3f299d128c91634255
vishalshaw26 <vishalshaw6826@gmail.com>
2026-09-07 11:45:45 +0000

    devcontainer

---
+++ .devcontainer/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+ console.log(3
+            +
+            3) //this code will work as ecpected but we need to prioritize clarity and readability
+
+let name = "vishal"
+let age = 20
+let isLoggedIn = false
+
+/* TYPES of  DataType
+number = 2^53
+bigint = for larger numbers
+string = "assembly of characteres"
+boolean = true/false
+null = stand alone value (type=object) or representation of empty value
+undefined = place hold for a value which is not defined
+symbol =  uniquness
+*/
+
+//OBJECT
+console.table([typeof name , age ,isLoggedIn   ]);
+
+
